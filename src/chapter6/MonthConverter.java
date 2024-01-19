06e0a64ea48b9cfe1c23bfea797a0d342b001141
Chigbogu Orji <brightorji60@gmail.com>
2024-01-19 11:13:29 +0100

    chore: print more bill info

--- src/chapter6/MonthConverter.java
+++ src/chapter6/MonthConverter.java
@@ -2,7 +2,7 @@ package chapter6;
 
 public class MonthConverter {
     public static void main(String[] arg){
-        System.out.println(Month.getMonth(1));
+        System.out.println(Month.getMonth(6));
         System.out.println(Month.getMonth("January"));
     }
 }
--- src/chapter6/PhoneBillCalculator.java
+++ src/chapter6/PhoneBillCalculator.java
@@ -3,7 +3,12 @@ package chapter6;
 public class PhoneBillCalculator {
     public static void main(String[] arg){
         PhoneBill bill = new PhoneBill(123);
-        bill.setUsedMinutes(4000);
+//        bill.setUsedMinutes(4000);
         bill.printBillingInformation();
+        System.out.println("Bill 1 Allotted minutes: " + String.format("%d", bill.getAllottedMinutes()));
+
+        PhoneBill bill2 = new PhoneBill(1234, 15, 200, 150);
+        bill2.printBillingInformation();
+        System.out.println("Bill 2 Allotted minutes: " + String.format("%d", bill2.getAllottedMinutes()));
     }
 }
