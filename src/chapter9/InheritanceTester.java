fd3a51b4baf89010604685ba5f79fbfcabdc00f0
Chigbogu Orji <brightorji60@gmail.com>
2024-01-19 11:15:08 +0100

    learn class inheritance

---
+++ src/chapter9/InheritanceTester.java
@@ -0,0 +1,9 @@
+package chapter9;
+
+public class InheritanceTester {
+    public static void main(String[] arg){
+//        Person person = new Person();
+        Employee employee = new Employee();
+    }
+
+}
